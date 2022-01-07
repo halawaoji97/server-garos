2057db92408b5596dc738abe8e4e2902c19304a2
halawaoji97 <ozidhalawa97@gmail.com>
2022-01-07 15:43:36 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 const methodOverride = require('method-override');
 const session = require('express-session');
@@ -54,6 +55,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set path for startbootstrap-sb-admin
